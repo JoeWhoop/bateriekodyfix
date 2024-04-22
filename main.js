0c7a1667da1dd720b904a4a5f94f780e59789962
JoeWhoop <147258204+JoeWhoop@users.noreply.github.com>
2024-04-22 14:28:53 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 document.getElementById("goButton").addEventListener("click", function () {
     var selectedMechanic = document.getElementById("mechanicDropdown").value;
+    var selectedStation = document.getElementById("stationDropdown").value;
 
     selectedMechanic = selectedMechanic.toUpperCase().replace(" ", "_");
+    selectedStation = selectedStation.toUpperCase().replace(" ", "_");
 
     var baseRedirectURL = "https://web.miniextensions.com/jnXiLi9iClv53yJm94W8?prefill_Mechanic=" + selectedMechanic;
 
     var redirectionURLs = {
-        "Mechanic": baseRedirectURL,
+        "A1": baseRedirectURL,
     };
 
     if (redirectionURLs[selectedStation]) {
