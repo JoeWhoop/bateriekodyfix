948a0727895754af6219a842fc3fc3a3087dc3f4
JoeWhoop <147258204+JoeWhoop@users.noreply.github.com>
2024-04-24 09:31:38 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,8 @@
 document.getElementById("goButton").addEventListener("click", function () {
     var selectedMechanic = document.getElementById("mechanicDropdown").value;
-    var selectedStation = document.getElementById("stationDropdown").value;
-
     selectedMechanic = selectedMechanic.toUpperCase().replace(" ", "_");
-    selectedStation = selectedStation.toUpperCase().replace(" ", "_");
 
     var baseRedirectURL = "https://web.miniextensions.com/jnXiLi9iClv53yJm94W8?prefill_Mechanic=" + selectedMechanic;
 
-    var redirectionURLs = {
-        "selectedMechanic": baseRedirectURL,
-    };
-
-    if (redirectionURLs[selectedStation]) {
-        window.location.href = redirectionURLs[selectedStation];
-    } else {
-        alert("Wybierz prawidlowe opcje.");
-    }
+    window.location.href = baseRedirectURL;
 });
